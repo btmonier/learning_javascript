4fffc4e6cf6ec9571d51a32417c9daa8cbbe3028
Brandon <brandon.monier@gmail.com>
2021-04-07 11:47:42 -0400

    Add new examples

--- 00_hello_world/index.js
+++ 00_hello_world/index.js
@@ -1,3 +1,12 @@
 // this is a comment
 console.log('hello world');
 
+
+// Variables
+let firstName = 'Brandon';
+console.log('Hello, ' + firstName);
+
+firstName = 'Brian'; // *can* overwrite
+console.log('Hello, ' + firstName);
+console.log('Pi equals ' + Math.PI);
+console.log('firstName class: ' + typeof firstName);
